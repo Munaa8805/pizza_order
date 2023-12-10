d2248f5234d16d21cc6f884c86b1e5709c542b60
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2023-12-09 23:05:52 -0800

    add header element

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,43 +7,45 @@ import css from "./header.module.css";
 
 function Header() {
     return (
-        <Navbar
-            collapseOnSelect
-            expand="lg"
-            className="bg-body-tertiary bg-light">
-            <Container>
-                <LinkContainer to="/">
-                    <Navbar.Brand>
-                        <p className={css.logo}>BC PIZZA</p>
-                    </Navbar.Brand>
-                </LinkContainer>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <LinkContainer to="/">
-                            <Nav.Item>Home</Nav.Item>
-                        </LinkContainer>
-                        <LinkContainer to="/menu">
-                            <Nav.Item>Menu</Nav.Item>
-                        </LinkContainer>
-                        <LinkContainer to="/about">
-                            <Nav.Item>About</Nav.Item>
-                        </LinkContainer>
-                        <LinkContainer to="/contact">
-                            <Nav.Item>Contact</Nav.Item>
-                        </LinkContainer>
-                    </Nav>
-                    <Nav>
-                        <LinkContainer to="/login" id="login">
-                            <Nav.Item>Login</Nav.Item>
-                        </LinkContainer>
-                        <LinkContainer to="/register">
-                            <Nav.Item id="register">Register</Nav.Item>
-                        </LinkContainer>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <header>
+            <Navbar
+                collapseOnSelect
+                expand="lg"
+                className="bg-body-tertiary bg-light">
+                <Container>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>
+                            <p className={css.logo}>BC PIZZA</p>
+                        </Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <LinkContainer to="/">
+                                <Nav.Item>Home</Nav.Item>
+                            </LinkContainer>
+                            <LinkContainer to="/menu">
+                                <Nav.Item>Menu</Nav.Item>
+                            </LinkContainer>
+                            <LinkContainer to="/about">
+                                <Nav.Item>About</Nav.Item>
+                            </LinkContainer>
+                            <LinkContainer to="/contact">
+                                <Nav.Item>Contact</Nav.Item>
+                            </LinkContainer>
+                        </Nav>
+                        <Nav>
+                            <LinkContainer to="/login" id="login">
+                                <Nav.Item>Login</Nav.Item>
+                            </LinkContainer>
+                            <LinkContainer to="/register">
+                                <Nav.Item id="register">Register</Nav.Item>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
     );
 }
 
